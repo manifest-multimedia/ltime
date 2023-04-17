e6cdd30e277a41a9f7e0f413c5df007cf206f733
Johnson Sebire <johnson@manifestghana.com>
2023-04-17 16:02:41 +0000

    Updated Navigation Links

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -11,26 +11,26 @@
                 <li class="current"><a href="/"><span>Home</span></a>
                    
                 </li>
-                <li class=""><a href="#"><span>Company</span></a>
+                <li class=""><a href="/our-company"><span>Company</span></a>
                    
                 </li> 
-                <li class=""><a href="#"><span>Services</span></a>
+                <li class=""><a href="/our-services"><span>Services</span></a>
                     
                 </li>
-                <li class=""><a href="#"><span>Projects</span></a>
+                <li class=""><a href="/our-projects"><span>Projects</span></a>
                     
                 </li> 
                 
-                <li><a href="contact.html"><span>Contact</span></a></li>   
+                <li><a href="/contact-us"><span>Contact</span></a></li>   
             </ul>
         </div>
     </nav>
 </div>
 <div class="menu-right-content clearfix">
     <div class="sign-box">
-        <a href="#"><i class="fas fa-user"></i>Sign In</a>
+        <a href="#register"><i class="fas fa-user"></i>Sign In</a>
     </div>
     <div class="btn-box">
-        <a href="#" class="theme-btn btn-one"><span>+</span>Become an Affiliate</a>
+        <a href="#affiliate" class="theme-btn btn-one"><span>+</span>Become an Affiliate</a>
     </div>
 </div>
