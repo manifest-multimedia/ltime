690427404e2cc65975d4995e9d74703dfa7cc83a
Johnson Sebire <johnson@manifestghana.com>
2023-04-18 04:51:59 +0000

    Added Deals Model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -541,7 +541,7 @@
                 <h2>Irresistible Deals</h2>
             </div>
             <div class="deals-carousel owl-carousel owl-theme dots-style-one owl-nav-none">
-                @foreach ($properties as $item)
+                @foreach (\App\Models\Deal::where('featured',true)->where('active', true)->limit(5)->get() as $item)
                 <div class="single-item">
                     <div class="row clearfix">
                         <div class="col-lg-6 col-md-6 col-sm-12 deals-block">
@@ -560,7 +560,7 @@
                                         <div class="price-box clearfix">
                                             <div class="price-info pull-left">
                                                 <h6>Price</h6>
-                                                <h4>$30,000.00</h4>
+                                                <h4>${{$item->price}}</h4>
                                             </div>
                                             {{-- <div class="author-box pull-right">
                                                 <figure class="author-thumb"> 
@@ -571,12 +571,12 @@
                                         </div>
                                         <p>{{$item->description}}</p>
                                         <ul class="more-details clearfix">
-                                            <li><i class="icon-14"></i>3 Beds</li>
-                                            <li><i class="icon-15"></i>2 Baths</li>
-                                            <li><i class="icon-16"></i>600 Sq Ft</li>
+                                            <li><i class="icon-14"></i>{{$item->beds}} Beds</li>
+                                            <li><i class="icon-15"></i>{{$item->baths}} Baths</li>
+                                            <li><i class="icon-16"></i>{{$item->squareft}} Sq Ft</li>
                                         </ul>
                                         <div class="other-info-box clearfix">
-                                            <div class="btn-box pull-left"><a href="property-details.html" class="theme-btn btn-one">See Details</a></div>
+                                            <div class="btn-box pull-left"><a href="#" class="theme-btn btn-one">Details</a></div>
                                             {{-- <ul class="other-option pull-right clearfix">
                                                 <li><a href="property-details.html"><i class="icon-12"></i></a></li>
                                                 <li><a href="property-details.html"><i class="icon-13"></i></a></li>
