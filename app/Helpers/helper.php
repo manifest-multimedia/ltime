ecacb051742f83d2bbec01a8ced220bdfa509d90
Johnson Sebire <johnson@manifestghana.com>
2023-05-10 18:34:10 +0000

    Referral Tracker Module Updated

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -5,10 +5,19 @@ namespace App\Helpers;
 class helper {
 
     public static function UpdateReferralCode($data) {
+
+        $session_data=session()->get('referred_by');
+
+        if(!$session_data===$data) {
+            session()->put('referred_by', $data);
+        }
         
-        // dd("data:$data");
+    }
 
+    public static function InitiateReferralTracking($data){
 
+        session()->put('referred_by', $data);
+        
     }
 
 }
--- app/Http/Middleware/TrackReferrals.php
+++ app/Http/Middleware/TrackReferrals.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Helpers\Helper;
+use App\Helpers\helper;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -19,25 +19,25 @@ class TrackReferrals
     public function handle(Request $request, Closure $next)
     {
 
-        // if($request->ref){
-        //     $referred_by=$request->ref;
+        if($request->ref){
 
-        //     $referred_by_user=DB::table('users')->where('affiliate_id', $referred_by)->first();
+            $referred_by=$request->ref;
+
+            $referred_by_user=DB::table('users')->where('affiliate_id', $referred_by)->first();
            
-        //     //Store information in the session; 
+            //Store information in the session; 
 
-        //     if(session()->has('referred_by')){
+            if(!session()->has('referred_by')){
 
-        //         $data = session()->get('referred_by');
+                helper::InitiateReferralTracking($referred_by);
 
-        //         Helper::UpdateReferralCode($data);
-
-
-        //     }
+            }else {
+                    helper::UpdateReferralCode($referred_by);
+            }
             
             
            
-        // }
+        }
 
         
 
