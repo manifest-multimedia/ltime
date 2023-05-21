cacdac087d09966a28ed82ba973569503297db75
Johnson Sebire <johnson@manifestghana.com>
2023-05-21 14:49:42 +0000

    Code Refactored | Added Auth Middleware to Insights Routes | Included Insights Routes in Route Service Provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,6 +42,9 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/jetstream.php'));
+
+                Route::middleware('web')
+                ->group(base_path('routes/insights.php'));
                
         });
             
