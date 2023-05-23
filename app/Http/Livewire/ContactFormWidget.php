2de21fcc339dc8dd1dc88b3e72ec7c15b9a9758b
Johnson Sebire <johnson@manifestghana.com>
2023-05-23 14:31:55 +0000

    Updated Contact Form Widget

--- app/Http/Livewire/ContactFormWidget.php
+++ app/Http/Livewire/ContactFormWidget.php
@@ -6,8 +6,34 @@ use Livewire\Component;
 
 class ContactFormWidget extends Component
 {
+    public $contactName;
+    public $contactEmail;
+    public $contactMessage;
+    public $contactPhone;
+    public $contactSubject; 
+
     public function render()
     {
         return view('livewire.contact-form-widget');
     }
+
+    public function sendMessage(){
+        $rules=[
+            'contactName' => 'required', 
+            'contactEmail'=> 'required', 
+            'contactPhone'=> 'required',
+            'contactMessage' => 'required', 
+        ];
+
+        $messages=[
+            'contactName.required' => 'How should we call you?', 
+            'contactEmail.required' => 'Please provide a valid email address', 
+            'contactPhone.required' => 'A valid phone number is required', 
+            'contactMessage.required' => 'The message field cannot be empty',
+        ];
+
+        $this->validate($rules,$messages);
+
+    }
+
 }
--- resources/views/livewire/contact-form-widget.blade.php
+++ resources/views/livewire/contact-form-widget.blade.php
@@ -4,19 +4,19 @@
         <form method="post" action="" id="contact-form"> 
             <div class="row clearfix">
                 <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                    <input type="text" name="username" placeholder="Your Name" required="">
+                    <input type="text" name="name" placeholder="Your Name" required="" wire:model="contactName">
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                    <input type="email" name="email" placeholder="Email address" required="">
+                    <input type="email" name="email" placeholder="Email address" required="" wire:model="contactEmail">
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                    <input type="text" name="phone" placeholder="Phone" required="">
+                    <input type="text" name="phone" placeholder="Phone" required="" wire:model="contactPhone">
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                    <input type="text" name="subject" placeholder="Subject" required="">
+                    <input type="text" name="subject" placeholder="Subject" required="" wire:model="contactSubject">
                 </div>
                 <div class="col-lg-12 col-md-12 col-sm-12 form-group">
-                    <textarea name="message" placeholder="Message"></textarea>
+                    <textarea name="message" placeholder="Message" wire:model="contactMessage"></textarea>
                 </div>
                 <div class="col-lg-12 col-md-12 col-sm-12 form-group message-btn">
                     <button class="theme-btn btn-one" type="submit" name="submit-form">Send Message</button>
